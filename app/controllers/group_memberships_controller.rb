f27a81d405eacfbb5a19297bc16784a951623fda
VenkatramanC <venkatraman.c9@gmail.com>
2015-12-03 11:32:58 +0530

    Member added to a group successfully

--- app/controllers/group_memberships_controller.rb
+++ app/controllers/group_memberships_controller.rb
@@ -1,4 +1,13 @@
 class GroupMembershipsController < ApplicationController
   def create
+    @group = Group.find(params[:group_membership][:group_id])
+    usr = User.find_by(email: params[:group_membership][:email])
+    if usr
+      @group.add(usr)
+     flash[:notice] = "Member Added"
+    else
+      flash[:alert] = "No Such user"
+    end
+    redirect_to @group
   end
 end
