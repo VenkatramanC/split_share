0509e023bdffa374fcfb3e21dd35044761b9e062
VenkatramanC <venkatraman.c9@gmail.com>
2015-12-03 14:33:58 +0530

    User Invite feature added

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'letter_opener'
 
 gem 'annotate'
 
+gem 'devise_invitable'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/group_memberships_controller.rb
+++ app/controllers/group_memberships_controller.rb
@@ -1,19 +1,26 @@
 class GroupMembershipsController < ApplicationController
   def create
     @group = Group.find(params[:group_membership][:group_id])
+     email = params[:group_membership][:email]
     usr = User.find_by(email: params[:group_membership][:email])
     if usr
       begin
         @group.add(usr)
       rescue
         flash[:notice] = "Already existed in group"
-      
-    else
-      flash[:notice] = "Member Added"
+      else
+        flash[:notice] = "Member Added"
       end
+
     else
-      flash[:alert] = "No Such user"
+      if email.match(Devise.email_regexp)
+        User.invite!(:email => email)
+        flash[:alert] = "No such User"
+      else
+        flash[:alert] = "Invalid E-Mail"
+      end
     end
+    
     redirect_to @group
   end
 end
